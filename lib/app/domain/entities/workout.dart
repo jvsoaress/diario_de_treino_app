e682898a9d88f5428b0e19db62c30f5161279ae4
João Vitor <j.soares@aparkado.de>
2022-03-25 00:22:42 +0100

    remove WorkoutId

--- lib/app/domain/entities/workout.dart
+++ lib/app/domain/entities/workout.dart
@@ -1,14 +1,12 @@
 import 'package:diario_de_treino_app/app/domain/entities/exercise.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
-import 'workout_id.dart';
-
 part 'workout.freezed.dart';
 
 @freezed
 class Workout with _$Workout {
   factory Workout({
-    required WorkoutId id,
+    required String id,
     required String title,
     required List<Exercise> exercises,
     required DateTime createdAt,
--- lib/app/domain/repositories/workout_repository.dart
+++ lib/app/domain/repositories/workout_repository.dart
@@ -1,7 +1,5 @@
 import 'package:diario_de_treino_app/app/domain/entities/workout.dart';
 
-import '../entities/workout_id.dart';
-
 abstract class WorkoutRepository {
-  Future<WorkoutId> create(Workout workout);
+  Future<void> create(Workout workout);
 }
