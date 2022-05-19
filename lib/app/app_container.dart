d7742b5614de65a7b0287ff0255a830e4cafd37d
João Vitor <j.soares@aparkado.de>
2022-05-19 17:27:14 -0300

    resolve dependencies

--- lib/app/app_container.dart
+++ lib/app/app_container.dart
@@ -2,6 +2,11 @@ import 'package:get_it/get_it.dart';
 
 import 'blocs/page_bloc.dart';
 import 'blocs/workout_bloc.dart';
+import 'data/repositories/workout_repository.dart';
+import 'data/repositories/workout_repository_impl.dart';
+import 'data/sql_database/sqflite_database.dart';
+import 'data/sql_database/sql_database.dart';
+import 'use_cases/save_workout_use_case.dart';
 
 class AppContainer {
   const AppContainer._();
@@ -11,9 +16,17 @@ class AppContainer {
   static T get<T extends Object>() => _getIt.get<T>();
 
   static void initDependencies() {
-    final pageBloc = PageBloc();
-    _getIt.registerFactory(() => pageBloc);
-    _getIt.registerFactory(() => WorkoutBloc(pageBloc: pageBloc));
+    _getIt.registerFactory(() => PageBloc());
+    _getIt.registerLazySingleton<SqlDatabase>(() => SqfliteDatabase());
+    _getIt.registerLazySingleton<WorkoutRepository>(
+      () => WorkoutRepositoryImpl(_getIt()),
+    );
+    _getIt.registerLazySingleton(
+      () => SaveWorkoutUseCase(repository: _getIt()),
+    );
+    _getIt.registerFactory(
+      () => WorkoutBloc(pageBloc: _getIt(), saveWorkoutUseCase: _getIt()),
+    );
   }
 
   static void reset() => _getIt.reset();
